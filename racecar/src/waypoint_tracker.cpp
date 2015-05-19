396ea5ec7bd7638e9b1c59dd0e70af31a057f5ef
florianhauer <flo@gatech.edu>
2015-05-19 11:15:58 -0400

    car neutral throtlle between -0.5 and 0.5 probably due to esc config #2 (50%)
    waypoint tracker updated to send throttle between 0.5 and 0.75

--- racecar/src/waypoint_tracker.cpp
+++ racecar/src/waypoint_tracker.cpp
@@ -66,7 +66,7 @@ int main(int argc, char **argv)
 			  throttle_pub.publish(zero);
 			  steering_pub.publish(zero);
 		  }else{
-			  filtered_throttle=filter_alpha*filtered_throttle+(1-filter_alpha)*sat(0.25*dist+0.2,0.5);
+			  filtered_throttle=filter_alpha*filtered_throttle+(1-filter_alpha)*sat(0.25*dist+0.5,0.75);
 			  std_msgs::Float64 throttle;
 			  throttle.data=filtered_throttle;
 			  throttle_pub.publish(throttle);
